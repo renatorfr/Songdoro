92e135b522035bee85f5c68ba266786043fe9c61
Renato Freire Ricardo <renatorfr@gmail.com>
2012-09-25 16:29:16 -0300

    Added Google Analytics

--- src/br/com/renatorfr/songdoro/SongdoroActivity.java
+++ src/br/com/renatorfr/songdoro/SongdoroActivity.java
@@ -3,11 +3,17 @@ package br.com.renatorfr.songdoro;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SongdoroActivity extends Activity {
+public class SongdoroActivity extends Activity
+{
 	/** Called when the activity is first created. */
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
+
+		// Google Analytics
+		GATracker.getInstance(getApplicationContext()).trackPageView("/" + this.getClass().getSimpleName());
+
 		setContentView(R.layout.main);
 	}
 }
